ed8ffbb02f69db5e4cb57edfefe8ab2dcba85867
Song-A <songa.lee@hutom.io>
2023-05-15 23:54:30 +0900

    Test and code gray background for disabled button

--- src/App.test.js
+++ src/App.test.js
@@ -46,3 +46,36 @@ test('checkbox disables button on first click and enables on second click', () =
   fireEvent.click(checkbox);
   expect(colorButton).toBeEnabled();
 })
+
+test('disabled button has gray background and reverts to red', () => {
+  render(<App />);
+
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  
+  // disable button
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'gray' });
+
+  // re-enable button
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+})
+
+test('clicked disabled button has gray background and reverts to blue', () => {
+  render(<App />);
+
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // change button to blue
+  fireEvent.click(colorButton);
+
+  // disable button
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'gray' });
+
+  // re-enable button
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+})
